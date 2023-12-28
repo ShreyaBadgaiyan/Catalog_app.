857da494975ce089a85d34ebda9d8d9064de8e3f
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2023-12-28 23:32:11 +0530

    Learned basic Animations

--- lib/screens/loginPage.dart
+++ lib/screens/loginPage.dart
@@ -11,21 +11,40 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // String name
+  String name="";
+  bool changeButton=false;
   @override
   Widget build(BuildContext context) {
     return Material(
       color:Colors.white,
       child:SingleChildScrollView(
-        child:Column(
+        child: Column(
           children:[
             Image.asset(
               "assets/Images/login.png",
-              fit:BoxFit.cover,
+fit: BoxFit.cover,
             ),
 
             SizedBox(
               height:20.0,
             ),
+
+
+            Text("Welcome $name",
+              style:
+              TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height:20.0,
+            ),
+
+
+
+
             Padding(
                 padding:const EdgeInsets.symmetric(
             vertical:16.0,horizontal: 32.0
@@ -33,10 +52,24 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               children:[
                 TextFormField(
+                  style: TextStyle(
+                    decoration: TextDecoration.none,
+                    decorationThickness: 0,
+                  ),
+
                   decoration:InputDecoration(
+                    focusedBorder: InputBorder.none,
                     hintText: "Enter Username",
                     labelText:"Username",
+
                   ),
+                  onChanged:(value){
+                    name=value;
+                    setState(() {
+
+                    });
+                  },
+
                 ),
 
                 SizedBox(
@@ -56,12 +89,57 @@ class _LoginPageState extends State<LoginPage> {
                   height:20.0,
                 ),
 
-                ElevatedButton(
-                  child:Text("Logina"),
-                  style:TextButton.styleFrom(minimumSize:Size(150,40)),
-                  onPressed: (){
+                // ElevatedButton(
+                //   child:Text("Login"),
+                //   style:TextButton.styleFrom(minimumSize:Size(150,40)),
+                //   onPressed: (){
+                //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                //   },
+                // )
+
+
+                InkWell(
+
+
+                  onTap: () async {
+                    setState(() {
+                      changeButton=true;
+                    });
+
+                    await Future.delayed(Duration(seconds:1));
                     Navigator.pushNamed(context, MyRoutes.homeRoute);
+
                   },
+                  child:
+                  AnimatedContainer(
+                    duration: Duration(seconds:1),
+                    width:changeButton?50:150,
+                    // height:50,
+
+                    alignment: Alignment.center,
+                    child: changeButton?Icon(
+                        Icons.done
+                    ,
+
+                    color: Colors.white,
+                    ):
+                    Text("Login",
+                    style:
+                      TextStyle(
+                        color:Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),),
+                    decoration: BoxDecoration(
+                      color: Colors.pink,
+                      borderRadius: BorderRadius.circular(changeButton?50:8),
+                      // shape: changeButton?
+
+                      // BoxShape.circle
+                      //     :BoxShape.rectangle,
+
+                    ),
+                  ),
                 )
 
 
